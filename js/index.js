6d2eed1ff083dbccbd25f8d057e412beef0f44bd
KIRSTEN-23 <231296@virtualwindow.co.za>
2023-05-28 20:48:20 +0200

    Exercise 3

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let timmy = 10
+let susan = 25
+let timmyOwe = 15
+let susanOwe = 26
+
+let timmyTax = timmyOwe / 100 * timmy
+let susanTax = susanOwe / 100 * susan
+
+let totalTax = timmyTax + susanTax
+
+console.log(totalTax)
